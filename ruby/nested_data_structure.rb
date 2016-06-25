a13dcf8c1903fd39b9434f654b35deaf8f62c115
Kari Anne <kariag@icloud.com>
2016-06-25 17:50:13 -0500

    Add arrays to hash

--- ruby/nested_data_structure.rb
+++ ruby/nested_data_structure.rb
@@ -1,2 +1,13 @@
 # Intialize empty hash
 house = {}
+
+# Add rooms in the house and list what is in them
+house = {
+	:living_room => ['couch', 'tv'],
+	:dining_room => ['table', 'hutch', 'fire_place'],
+	:kitchen => ['refridgerator', 'sink', 'dishwasher'],
+	:bedroom_main => ['king_bed', 'dresser', 'mirror', 'closet'],
+	:guest_bedroom => ['queen_bed', 'dresser', 'vanity'],
+	:bathroom => ['bath-tub', 'shower', 'shelves']
+	}
+p house
