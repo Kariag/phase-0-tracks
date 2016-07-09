749d10eb32058db7c3d9a383e9d5aa153ac4e7e0
Kari Anne <kariag@icloud.com>
2016-07-09 14:47:30 -0500

    Add a speak method for santa

---
+++ ruby/santa.rb
@@ -0,0 +1,18 @@
+# A speak method that will print "Ho, ho, ho! Haaaappy holidays!" (Santa has been working on being more inclusive.)
+# An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!"
+# An initialize method that prints "Initializing Santa instance ...".
+# Add a bit of code below your class declaration to check that you're able to initialize a Santa instance and call its methods.
+
+class Santa
+	def speak
+		puts "Ho,ho,ho! Haaaapppy holidays!"
+	end
+
+	def eat_milk_and_cookies(type)
+		puts "That was a good #{type}!"
+	end
+end
+
+Mr_Clause = Santa.new
+Mr_Clause.speak
+Mr_Clause.eat_milk_and_cookies("snickerdoodle")
