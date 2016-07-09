7aa7830d9494cbc3321c508b7688890b8b41c38d
Kari Anne <kariag@icloud.com>
2016-07-09 13:50:06 -0500

    characteristics and behaviors added

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,1 +1,18 @@
-lsdjfoasjd
+Species ------------ Dog
+
+Characteristics ---------------
+Leg count: 4
+Eye count: 2
+Ear count: 2
+Friendly : yes
+Name : varies
+Color: varies
+
+
+Behavior ----------------
+Sit
+Shake
+Rolls over
+Play fetch
+Catch frisbee
+Chase ball
