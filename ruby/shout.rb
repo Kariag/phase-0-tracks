ce81795ae080ef83f1d2000a9b5a27708bb1df85
Kari Anne <kariag@icloud.com>
2016-07-13 14:12:35 -0400

    Add a mixin version of Shout module add two classes representing mom and dad and add driver code to call module methods on each instance

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -4,16 +4,39 @@
 # Add driver code underneath your module declaration
 # that calls both of your module methods
 
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#
+#   def self.yelling_happily(happy_words)
+#     happy_words + ":):):)" + "!"
+#   end
+# end
+#
+# p Shout.yell_angrily('Bullshit')
+# p Shout.yelling_happily('I love you')
 
+# Write two classes representing anything that might shout,
+# and include the Shout module in those classes.
+# Test your work by adding driver code at the bottom of the file that instantiates
+# instances of your classes and calls the two module methods on each instance.
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yelling_happily(happy_words)
-    happy_words + ":):):)" + "!"
+  def yell(words)
+    words.upcase + "!!!!"
   end
 end
 
-p Shout.yell_angrily('Bullshit')
-p Shout.yelling_happily('I love you')
+class Mom
+  include Shout
+end
+
+class Dad
+  include Shout
+end
+
+mom = Mom.new
+p mom.yell('I told you to clean up your room')
+
+dad = Dad.new
+p dad.yell('I told you to listen to your mother')
