35364a4eeee2ec99c8373391c042c684c6c1dd65
Kari Anne <kariag@icloud.com>
2016-07-13 13:53:18 -0400

    Add Flight module to file

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,5 +1,11 @@
 #Declare classes for a bird and a plane
 
+module Flight
+  def take_off(altitude)
+    puts "Taking off and ascending until reaching #{altitude} ..."
+  end
+end
+
 class Bird
 end
 
